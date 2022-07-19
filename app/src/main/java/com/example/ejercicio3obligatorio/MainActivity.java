401950d214cd4e621491583780a5a766b0185263
pozito31 <jessicapozo198715@gmail.com>
2022-07-19 19:30:35 +0200

    Actualizar

--- app/src/main/java/com/example/ejercicio3obligatorio/MainActivity.java
+++ app/src/main/java/com/example/ejercicio3obligatorio/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView fotoPerro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fotoPerro = findViewById(R.id.fotoPerro);
 
         //TODO DEBERIAMOS CONTROLAR QUE HAY ACCESO A ITERNET
         DescargaAnimales descargaAnimales = new DescargaAnimales(this);
@@ -20,5 +26,6 @@ public class MainActivity extends AppCompatActivity {
     public void mostrarAnimalRecibido(InfoAnimales infoAnimales) {
         Log.d("ETIQUETA_LOG" ,"Perro rx = " + infoAnimales.toString());
         //TODO USAR PICASO PARA LA URL DE LA IMAGEN QUE RECIBO, CARGARLA EN UN IMAGEVIEW
+        Picasso.get().load(infoAnimales.getMessage()).into(fotoPerro);
     }
 }
